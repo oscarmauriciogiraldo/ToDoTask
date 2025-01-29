acdd710e280921deec01a98938881be0c10785bb
OscarGiraldo <oscar.giraldo@linktic.com>
2025-01-29 15:02:51 -0500

    chore: add Eslint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,8 +8,14 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecamVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
   plugins: ['react-refresh'],
   rules: {
+    "react/react-in-jsx-scope": "off",
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
