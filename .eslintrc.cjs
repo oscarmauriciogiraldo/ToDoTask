b642902d954639de3d455830ca265dea51e94ed9
OscarGiraldo <oscar.giraldo@linktic.com>
2025-01-29 18:48:53 -0500

    feat: FC list Todo tasks

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,29 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  //root: true,
+  env: { 
+    browser: true, 
+    es2021: true 
+  },
   extends: [
-    'eslint:recommended',
+    'plugin:react/recommended',
+    'standard-with-typescript'
+    /* 'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:react-hooks/recommended', */
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
+  overrides: [
+  ],
+  //ignorePatterns: ['dist', '.eslintrc.cjs'],
+  //parser: '@typescript-eslint/parser',
   parserOptions: {
     ecamVersion: 'latest',
     sourceType: 'module',
     project: './tsconfig.json'
   },
-  plugins: ['react-refresh'],
+  plugins: ['react'],
   rules: {
-    "react/react-in-jsx-scope": "off",
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    
   },
 }
